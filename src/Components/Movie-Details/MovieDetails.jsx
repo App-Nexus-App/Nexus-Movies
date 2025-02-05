0633a3420049ae4c07643ccfa2d460eb7b5a123b
Phumlani-Mabena <phumlanimabena2@gmail.com>
2025-02-05 11:48:31 +0200

    fix: correct spelling of "IMDb" in MovieDetails component

--- src/Components/Movie-Details/MovieDetails.jsx
+++ src/Components/Movie-Details/MovieDetails.jsx
@@ -116,7 +116,7 @@ const MovieDetails = () => {
         <div className="movie-content">
           <h2>{movie.title}</h2>
           <p><strong>Description: </strong>{movie.overview}</p>
-          <p className="rating"><strong>IMBD Ratings⭐ :</strong>  {movie.vote_average.toFixed(1)}/10</p>
+          <p className="rating"><strong>IMDb Ratings⭐ :</strong>  {movie.vote_average.toFixed(1)}/10</p>
           <p className="release-date"> <strong>Release Date📅 :</strong>  {movie.release_date}</p>
         </div>
       </div>
