8d52dc77575c7e9ccca493d0e2a76c975c8a4337
Lucky Sibuyi <Lucky1sibuyi@gamil.com>
2025-02-06 09:31:00 +0200

    update heading

--- src/Components/Movie-Details/MovieDetails.jsx
+++ src/Components/Movie-Details/MovieDetails.jsx
@@ -158,7 +158,7 @@ const MovieDetails = () => {
       {/* Suggested Movies by Same Actors */}
       {actorMovies.length > 0 && (
         <div className="suggested-movies">
-          <h3>🎭 More Movies Featuring {actorMovies[0]?.name}</h3>
+          <h3>You may also: {actorMovies[0]?.name}</h3>
           <div className="movies-grid">
             {actorMovies.map((actorMovie) => (
               <Link 
